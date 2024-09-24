a68beb89dacf8dcef39df044c15e86d0e856d10c
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-09-24 14:14:44 -0600

    ok

--- traficlight/sketch.js
+++ traficlight/sketch.js
@@ -1,15 +1,33 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+let ms = 0;
+
+s = 1;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  drawOutlineOfLights();
+}
+
+function drawOutlineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+
+  //lights
+  fill(255);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+  ellipse(width/2, height/2, 50, 50); //middle
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
+
+function change(){
+  if (s = 1){s=2}
+  if (s=2){s=3}
+  if (s= 3){s=1}
+
 }
