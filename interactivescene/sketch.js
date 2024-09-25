90dc32ee47846cfba931c7fcff231f7488e251a3
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-09-25 14:15:45 -0600

    Update sketch.js

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -8,16 +8,16 @@
 
 let x = 200;
 let y = 200;
-let r = 25
-let dx=4
+let r = 25;
+let dx=4;
 
 
-let bgColor= 220
+let bgColor= 220;
 let fill_colorR = 0;
 let fill_colorG = 0;
 let fill_colorB = 0; 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(windowWidth,windowHeight);
   background(bgColor);
 }
 
@@ -25,68 +25,71 @@ function setup() {
 function draw() {
   
 
-  x = mouseX
-  y = mouseY
-  
-  // multicolor()
-  changecolor()
+  x = mouseX;
+  y = mouseY;
+  noStroke();
+
+
 }
 
 function mouseWheel(event){
 
-  direction = event.delta
+  let direction = event.delta;
   
 
-  noStroke()
-  circle(x,y,r*2)
+  noStroke();
+  circle(x,y,r*2);
   if (direction>0&&r>2){
-    r = r-5
+    r = r-5;
   }
   else{
-    r=r+5
+    r=r+5;
   }
 }
 
 function mouseDragged(){
   if (mouseButton===LEFT){
-    fill(fill_colorR,fill_colorG,fill_colorB)
-    circle(x,y,r*2)
+    fill(fill_colorR,fill_colorG,fill_colorB);
+    circle(x,y,r*2);
   }
   if (mouseButton===RIGHT){
-    noStroke()
-    fill(bgColor)
-    circle(x,y,r*2)
+    noStroke();
+    fill(bgColor);
+    circle(x,y,r*2);
     
   }
 }
 
 function keyPressed(){
-  if (key==='e'){
-    clear()
-    setup()
+  if (key==="e"){
+    clear();
+    setup();
   }
-  if (key==='h'){
+  if (key==="h"){
     
-    rect(0,mouseY,wCanva,r)
+    rect(0,mouseY,windowWidth,r);
   }
-  if (key==='v'){
+  if (key==="v"){
     
-    rect(mouseX,0,r,hCanva)
+    rect(mouseX,0,r,windowHeight);
   }
-  //  if (key === LEFT_ARROW){
-  //     changecolor()
-  //  }
+
+  if (key==='c'){
+    changecolor();
+  }
+
 }
 
-function multicolor(){
-  
-  fill_colorR = Math.sqrt(mouseX**2+mouseY**2)/400*255
-  fill_colorG = Math.sqrt((400-mouseX)**2+(400-mouseY)**2)/400*255
-  fill_colorB = Math.sqrt((mouseX)**2+(400-mouseY)**2)/400*255
-}
+
 
 function changecolor(){
-  fill_colorR = random(0,255)
-  fill_colorB = random(0,255)
-  fill_colorG = random(0,255)
+  fill_colorR = random(0,255);
+  fill_colorB = random(0,255);
+  fill_colorG = random(0,255);
 }
+
+// function changeR(){
+//   if (keyIsPressed()==="1"){fill_colorR = random(0,255)}
+  
+// }
+
