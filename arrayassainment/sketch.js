8fa0f7fe153ce5d9e34e519f9bbce825b4439334
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-10-09 16:53:02 -0600

    Update sketch.js

--- arrayassainment/sketch.js
+++ arrayassainment/sketch.js
@@ -6,77 +6,45 @@
 // - describe what you did to take this project "above and beyond"
 const SPAWNX = 0;
 const SPAWNY = 400;
-let ms = 40;
-let hp = 468;
-let apr = 20;
-let adr = 20;
-let ad = 70;
-let ap = 0;
-let player;
-let startmoving = false;
-let pX;
-let pY;
-let vx;
-let vy;
-let desti = [];
-let moving = false;
+
+//let hp = 468;
+//let apr = 20;
+//let adr = 20;
+//let ad = 70;
+//let ap = 0;
+let position;
+let destination;
+let speed = 3.75;
+
+
 function preload(){
-  player = loadImage("akali.png");
+  player = loadImage("akali.png")
 }
 function setup() {
-  
-  createCanvas(windowWidth, windowHeight);
-  
-  spawnchampion();
-  
+  createCanvas(windowWidth,windowHeight);
+  position = createVector(SPAWNX, SPAWNY); 
+  destination = createVector(copy); 
 }
 
 function draw() {
   background(220);
-  displayP();
-
-}
-
-function keyPressed(){
-  if (key === 'a'){
-    startmoving = true;
-    moveIni();
-  }
+  
 
   
+  move()
+  image(player,position.x,position.y)
+ 
 }
 
-function moveP(){
-  if  (moving === true ){
-    while (pX!==desti.destiX&&pY!==desti.destiY){
-      let sum = desti.destiX-pX+(desti.destiY-pY);
-      pX = (desti.destiX-pX)/abs.sum*ms;
-      pY += (desti.destiY-pY)/abs.sum*ms;
-    }
+function mousePressed() {
+ 
+  destination.set(mouseX, mouseY);
+}
 
+function move(){
+  let direction = p5.Vector.sub(destination, position);
+  if (direction.mag() > 1) {
+    direction.setMag(speed);
+    position.add(direction); 
   }
-}
-
-
-function displayP(){
-  image(player,pX,pY);
-  moveP();
-}
-function spawnchampion(){
-  image(player,SPAWNX,SPAWNY);
-  pX = SPAWNX;
-  pY = SPAWNY;
-}
-
-
-
-
-function moveIni(){
-  let destination = {
-    destiX: mouseX,
-    destiY:mouseY,
-  };
-  desti.push(destination);
-  startmoving = false;
-  moving = true;
 }
