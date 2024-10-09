be8cc051826119a2fe197a14bc90fd8a70cebdad
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-10-09 14:15:40 -0600

    Update sketch.js

--- arrayassainment/sketch.js
+++ arrayassainment/sketch.js
@@ -6,45 +6,77 @@
 // - describe what you did to take this project "above and beyond"
 const SPAWNX = 0;
 const SPAWNY = 400;
-let ms = 3.85;
-let hp = 468
-let apr = 20
-let adr = 20
-let ad = 70
-let ap = 0
+let ms = 40;
+let hp = 468;
+let apr = 20;
+let adr = 20;
+let ad = 70;
+let ap = 0;
 let player;
-let move = false;
+let startmoving = false;
 let pX;
 let pY;
-
+let vx;
+let vy;
+let desti = [];
+let moving = false;
 function preload(){
-  player = loadImage("akali.png")
+  player = loadImage("akali.png");
 }
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
   
-  spawnchampion()
+  spawnchampion();
   
 }
 
 function draw() {
   background(220);
-  image(player,pX,pY)
+  displayP();
+
 }
 
-function mouseClicked(){
-  if (mouseButton === RIGHT){
-    move = true
-
+function keyPressed(){
+  if (key === 'a'){
+    startmoving = true;
+    moveIni();
   }
+
   
 }
 
+function moveP(){
+  if  (moving === true ){
+    while (pX!==desti.destiX&&pY!==desti.destiY){
+      let sum = desti.destiX-pX+(desti.destiY-pY);
+      pX = (desti.destiX-pX)/abs.sum*ms;
+      pY += (desti.destiY-pY)/abs.sum*ms;
+    }
+
+  }
+}
 
 
+function displayP(){
+  image(player,pX,pY);
+  moveP();
+}
 function spawnchampion(){
-  image(player,SPAWNX,SPAWNY)
-  pX = SPAWNX
-  pY = SPAWNY
+  image(player,SPAWNX,SPAWNY);
+  pX = SPAWNX;
+  pY = SPAWNY;
+}
+
+
+
+
+function moveIni(){
+  let destination = {
+    destiX: mouseX,
+    destiY:mouseY,
+  };
+  desti.push(destination);
+  startmoving = false;
+  moving = true;
 }
