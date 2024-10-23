8e7289734ef11c0c6b35790a262d8ec054fe0511
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-10-23 13:56:43 -0600

    Update sketch.js

--- gridemo/sketch.js
+++ gridemo/sketch.js
@@ -1,56 +1,94 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-const GRIDSIZE = 300
-let cellsize;
-// let grid = [
-//   [1,0,1,0],
-//   [0,0,1,1],
-//   [1,1,1,0],
-//   [0,1,1,0],]
-
 let grid;
-
+const GRID_SIZE = 20;
+let cellSize;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cellsize = height/GRIDSIZE
-  grid = generategrid(GRIDSIZE,GRIDSIZE)
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
   background(220);
-  display()
+  displayGrid();
 }
 
-function display(){
-  for (let y=0;y<GRIDSIZE;y++){
-    for(let x =0;x<GRIDSIZE;x++){
-      if(grid[y][x]===0){
-        fill("black")
-      }
-      else if(grid[y][x]===1  ){
-        fill("white")
-      }
-      square(x*cellsize,y*cellsize,cellsize)
-    }
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
   }
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
   }
+}
 
-function generategrid(column,rows){
-  let newgrid = []
-  for (let y=0;y<rows;y++){
-    newgrid.push([])
-    for (let x=0;x<column;x++){
-      if(random(100)<50){
-        newgrid[y].push(1)
+
+function mousePressed(){
+  let x = Math.floor(mouseX/cellSize)
+  let y = Math.floor(mouseY/cellSize)
+  
+  flip(x-1,y)
+  flip(x+1,y)
+  flip(x,y)
+  flip(x,y-1)
+  flip(x,y+1)
+}
+
+
+function flip(x,y){
+  if(x>=0&&x<GRID_SIZE&&y>=0,y<GRID_SIZE){
+
+    if (grid[y][x]===1){
+      grid[y][x]=0
+    }
+    else{grid[y][x]=1}
+  }
+}
+
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
       }
-      else{newgrid[y].push(0)}
+      else if (grid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
     }
   }
-  return newgrid
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      //choose either 0 or 1, each 50% of the time
+      if (random(100) < 50) {
+        newGrid[y].push(1);
+      }
+      else {
+        newGrid[y].push(0);
+      }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
