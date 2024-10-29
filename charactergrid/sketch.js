5260f013edf7a3c06790a17829f0cb6e2e4003aa
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-10-29 13:15:15 -0600

    f

---
+++ charactergrid/sketch.js
@@ -0,0 +1,96 @@
+let grid;
+const GRID_SIZE = 20;
+let cellSize;
+
+function setup() {
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+}
+
+
+function mousePressed(){
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+  
+  flip(x-1,y);
+  flip(x+1,y);
+  flip(x,y);
+  flip(x,y-1);
+  flip(x,y+1);
+}
+
+
+function flip(x,y){
+  if(x>=0&&x<GRID_SIZE&&y>=0,y<GRID_SIZE){
+
+    if (grid[y][x]===1){
+      grid[y][x]=0;
+    }
+    else{
+      grid[y][x]=1;
+    }
+  }
+}
+
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else if (grid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      //choose either 0 or 1, each 50% of the time
+      if (random(100) < 50) {
+        newGrid[y].push(1);
+      }
+      else {
+        newGrid[y].push(0);
+      }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
+}
