85d18fb3f0747d3eb6b44ee5954f0635cb9637c0
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-10-30 14:15:34 -0600

    Update sketch.js

--- gridassainment/sketch.js
+++ gridassainment/sketch.js
@@ -9,28 +9,62 @@ let rows = 10;
 let cellSize = 40; 
 let grid = []; 
 let minesCount = 10;
-const blank = 0
-
+let blank = 0
+const MINES = 25
+let mines = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  grid = gridinitial(cols,rows)
+  displaygridinitial()
+  mines = generatemines()
 }
 
 function draw() {
-  background(220);
+  
+  displaygrid()
+}
+
+function mousePressed(){
+  generatemines()
 }
 
 
-function gridbackground(cols, rows) {
+
+
+function gridinitial(cols, rows) {
   let arr = [];
   for (let y = 0; y < rows; y++) {
     arr.push([])
     for (let x = 0; x < cols; x++){
-      newGrid[y].push(blank)
+      arr[y].push(blank)
     }
   }
   return arr;
 }
 
+function displaygridinitial(){
+  for (let y=0;y<rows;y++){
+    for (let x=0;x<cols;x++){
+      if (grid[y][x]===blank){
+        fill("green")
+      }
+      // if(grid[y][x]===mine){
 
+      // }
+      square(x*cellSize,y*cellSize,cellSize)
+    }
+  }
+}
+
+
+
+function generatemines(){
+  let arr = []
+  for (let i=0;i<MINES;i++){
+    let x = floor(random(0,10))
+    let y = florr(random(0,10))
+    grid[y][x]===1
+  }
+  
+}
