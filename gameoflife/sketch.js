a4ff6d0c1d6b3b3a6c443efb2541e228d9c8fc11
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-11-03 15:19:09 -0600

    countmines
    tomorrow add display # of mines, change all blank to const with caps

--- gameoflife/sketch.js
+++ gameoflife/sketch.js
@@ -1,181 +1,110 @@
-
-let autoplay = false
-let grid;
-const GRID_SIZE = 40;
-let cellSize;
-let shouldToggleNeighbours = false;
-let renderOnFrame = 2;
-let gosper;
-
-
-function preload(){
-  gosper = loadJSON("gosper.json")
-}
-
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let cols = 10; 
+let rows = 10; 
+let cellSize = 40; 
+let grid = []; 
+let minesCount = 10;
+let blank = 0
+const MINES = 25
+let mines = []
+const MINE = 1
 function setup() {
-  if (windowWidth < windowHeight) {
-    createCanvas(windowWidth, windowWidth);
-  }
-  else {
-    createCanvas(windowHeight, windowHeight);
-  }
-  cellSize = height/GRID_SIZE;
-  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
-}
-
-function windowResized() {
-  if (windowWidth < windowHeight) {
-    resizeCanvas(windowWidth, windowWidth);
-  }
-  else {
-    resizeCanvas(windowHeight, windowHeight);
-  }
-  cellSize = height/GRID_SIZE;
+  createCanvas(windowWidth, windowHeight);
+  grid = gridinitial(cols,rows)
+  displaygridinitial()
+  generatemines()
 }
 
 function draw() {
-  background(220);
-  if (autoplay&&frameCount%renderOnFrame===0){
-    grid = updateGrid()
-  }
-  displayGrid();
+
+  displaygridinitial()
+  countMines()
 }
 
-function mousePressed() {
+function mousePressed(){
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
-
-  //toggle self
-  toggleCell(x, y);
-
-  //toggle neighbours
-  if (shouldToggleNeighbours) {
-    toggleCell(x + 1, y);
-    toggleCell(x - 1, y);
-    toggleCell(x, y + 1);
-    toggleCell(x, y - 1);
+  if (grid[y][x]===MINE){
+    gameover()
   }
 }
 
-function toggleCell(x, y) {
-  //make sure the cell you're toggling is in the grid
-  if (x >= 0 && y >= 0 && x < GRID_SIZE && y < GRID_SIZE) {
-    if (grid[y][x] === 1) {
-      grid[y][x] = 0;
-    }
-    else {
-      grid[y][x] = 1;
-    }
-  }
-}
 
-function keyPressed() {
-  if (key === "r") {
-    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
-  }
-  if (key === "e") {
-    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
-  if (key === "n") {
-    shouldToggleNeighbours = !shouldToggleNeighbours;
-  }
-  if (key === " ") {
-    grid = updateGrid();
-  }
-  if (key==='a'){
-    autoplay = !autoplay
-  }
 
-  if (key==="g"){
-    grid = gosper
-  }
-}
 
-function updateGrid() {
-  //make a new array to hold the next turn
-  let nextTurn = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
 
-  //look at every cell
-  for (let y = 0; y < GRID_SIZE; y++) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-      //count it's neighbours
-      let neighbours = 0;
-
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          //don't fall of the edge
-          if (y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE) {
-            neighbours += grid[y+i][x+j];
-          }
-        }
-      }
-
-      //don't count yourself
-      neighbours -= grid[y][x];
-
-      //apply the rules 
-      if (grid[y][x] === 0) {
-        //currently dead
-        if (neighbours === 3) {
-          nextTurn[y][x] = 1;
-        }
-        else {
-          nextTurn[y][x] = 0;
-        }
-      }
-
-      if (grid[y][x] === 1) {
-        //currently alive
-        if (neighbours === 2 || neighbours === 3) {
-          nextTurn[y][x] = 1;
-        }
-        else {
-          nextTurn[y][x] = 0;
-        }
-      }
-    }
-  }
-  return nextTurn;
-}
-
-function displayGrid() {
-  for (let y = 0; y < GRID_SIZE; y++) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-      if (grid[y][x] === 1) {
-        fill("black");
-      }
-      else if (grid[y][x] === 0) {
-        fill("white");
-      }
-      square(x * cellSize, y * cellSize, cellSize);
-    }
-  }
-}
-
-function generateRandomGrid(cols, rows) {
-  let newGrid = [];
+function gridinitial(cols, rows) {
+  let arr = [];
   for (let y = 0; y < rows; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x++) {
-      //choose either 0 or 1, each 50% of the time
-      if (random(100) < 50) {
-        newGrid[y].push(1);
-      }
-      else {
-        newGrid[y].push(0);
-      }
+    arr.push([])
+    for (let x = 0; x < cols; x++){
+      arr[y].push(blank)
     }
   }
-  return newGrid;
+  return arr;
 }
 
-function generateEmptyGrid(cols, rows) {
-  let newGrid = [];
-  for (let y = 0; y < rows; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x++) {
-      newGrid[y].push(0);
+function displaygridinitial(){
+  for (let y=0;y<rows;y++){
+    for (let x=0;x<cols;x++){
+      if (grid[y][x]===blank){
+        fill("green")
+      }
+      else if(grid[y][x]===MINE){
+        fill ('blue')
+      }
+      square(x*cellSize,y*cellSize,cellSize)
     }
   }
-  return newGrid;
+}
+
+
+
+function generatemines(){
+  let placedMines = 0
+  while (placedMines < MINES) {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    if (grid[y][x] !== MINE) {
+      grid[y][x] = MINE;
+      placedMines +=1;
+    }
+  }
+  
+}
+
+function countMines(){
+  for (let y =0;y<rows;y++){
+    for (let x=0;x<cols;x++){
+      if (grid[y][x]===blank){
+        n = count(y-1,x-1)+count(y-1,x)+count(y-1,x+1)+count(y,x-1)+count(y,x+1)+count(y+1,x-1)+count(y+1,x+1)+count(y+1,x)
+        
+        //#then display n #of mines
+      }
+    }
+  }
+}
+
+
+function count(y,x){
+  if (x >= 0 && y >= 0 && x < cols && y < rows) {
+    if (grid[y][x]===MINE){
+      return MINE
+    }
+  }
+  else{return blank}
+}
+
+
+
+
+
+function gameover(){
+  background(220)
+  noLoop()
 }
