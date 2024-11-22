b08a8f2ad5f864ff35e9cda0f865ca3ab4293b1c
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-11-22 13:30:45 -0600

    d

--- collide/sketch.js
+++ collide/sketch.js
@@ -1,15 +1,42 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+let hit = false;
+const poly = []; // stores the vertices for our polygon.
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 600);
+  collideDebug(true); // enable debug mode
+
+  // Set x,y positions as vecs:
+  poly[0] = createVector(323, 431);
+  poly[1] = createVector(210, 311);
+  poly[2] = createVector(220, 223);
+  poly[3] = createVector(590, 233);
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  // Draw the polygon by iterating over the 4 created vectors{x, y} stored in poly[]:
+  beginShape();
+  for (const { x, y } of poly)  {
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+
+  rect(mouseX, mouseY, 45, 100);
+
+  hit = collideRectPoly(mouseX, mouseY, 45, 100, poly);
+
+  // Enable the hit detection if the rectangle is wholly inside the polygon:
+  // hit = collideRectPoly(mouseX, mouseY, 45, 100, poly, true);
+
+  // Use vectors as input:
+  // const mouse     = createVector(mouseX, mouseY);
+  // const rect_size = createVector(45, 100);
+
+  // hit = collideRectPolyVector(mouse, rect_size, poly);
+  // Or:
+  // hit = collideRectPolyVector(mouse, rect_size, poly, true);
+
+  stroke(hit ? color('red') : 0);
+  print('colliding?', hit);
 }
